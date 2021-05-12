45bbe3f6d58a7e593ba81edfce8ce881305e97cd
Starlight13 <o.romanishina@gmail.com>
2021-05-12 10:31:00 +0300

    Init

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by dsadas asdasd on 24.03.2021.
+//  Copyright © 2021 dsadas asdasd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
