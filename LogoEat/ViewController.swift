476309caf416cb850e3425d543278c7ddae6be0e
Starlight13 <o.romanishina@gmail.com>
2021-05-16 18:18:51 +0300

    "Created table view with custom cell"

---
+++ LogoEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogoEat
+//
+//  Created by dsadas asdasd on 15.05.2021.
+//  Copyright © 2021 dsadas asdasd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
